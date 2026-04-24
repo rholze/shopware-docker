134429f4ea0c763c40678fb46b624eef9754ad2e
rholze <ronald.holze@gmx.de>
2026-04-24 16:55:01 +0200

    Set APP_URL for https

--- install.sh
+++ install.sh
@@ -14,6 +14,13 @@ if [ ! -f /var/www/html/platforms.php ] || [ "$FORCE_INSTALL" = "1" ]; then
     chmod -R 777 var/cache var/log public/thumbnail public/media public/robots.txt
     chown -R www-data:www-data /var/www/html
 
+    rm -f .env
+    touch .env
+    echo "APP_URL=https://shopware.myhobby-ki.de" >> .env
+    echo "APP_ENV=prod" >> .env
+    echo "DATABASE_URL=mysql://shopware:shopware@db:3306/shopware" >> .env
+    echo "REDIS_URL=redis://redis:6379" >> .env
+
     php -d memory_limit=512M bin/console asset:install --no-interaction
     php -d memory_limit=512M bin/console build:theme --no-interaction || true
 
