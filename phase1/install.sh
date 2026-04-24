0c6369790e8c8d8a653fe8648f1b914e82b259ea
rholze <ronald.holze@gmx.de>
2026-04-24 18:55:24 +0200

    Add debug output

--- phase1/install.sh
+++ phase1/install.sh
@@ -2,9 +2,11 @@
 
 set -e
 
+echo ">>> FORCE_INSTALL=$FORCE_INSTALL"
+
 php-fpm &
 
-sleep 3
+sleep 5
 
 if [ "$FORCE_INSTALL" = "1" ]; then
     echo ">>> Shopware installation..."
@@ -12,12 +14,12 @@ if [ "$FORCE_INSTALL" = "1" ]; then
     rm -rf /var/www/html/*
     rm -rf /var/www/html/.*
     
-    composer create-project shopware/production /var/www/html --no-interaction
+    composer create-project shopware/production /var/www/html --no-interaction || true
     
     cd /var/www/html
     
     mkdir -p var/cache var/log public/thumbnail public/media
-    chmod -R 777 var/cache var/log public/thumbnail public/media public/robots.txt
+    chmod -R 777 var/cache var/log public/thumbnail public/media public/robots.txt 2>/dev/null || true
     
     php -d memory_limit=512M bin/console asset:install --no-interaction || true
     
